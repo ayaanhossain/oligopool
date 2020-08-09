a9a87cabf0bc7e2a2ec420354d52e7c2d8ece914
Ayaan Hossain <ain.hoss07@gmail.com>
2020-08-09 01:25:33 -0400

    barcode update, intro to utils

---
+++ opcalc/utils.py
@@ -0,0 +1,89 @@
+
+import numpy as np
+
+
+# Decorators
+
+def coroutine(func):
+    '''
+    Decorator used to prime a coroutine.
+    Internal use only.
+
+    :: func
+       type - coroutine
+       desc - a coroutine we want online
+    '''
+
+    def online(*args, **kwargs):
+        _coroutine = func(*args, **kwargs)
+        next(_coroutine)
+        return _coroutine
+    
+    return online
+
+# Printing / Logging
+
+@coroutine
+def liner_engine(online=True):
+    '''
+    Return a coroutine to print stuff
+    to stdout and log run information.
+    Internal use only.
+
+    :: online
+       type - boolean
+       desc - if True will print received
+              strings to stdout
+              (default=True)
+    '''
+    
+    # Book-keeping
+    clrlen = 0
+
+    # Online
+    try:
+        while True:
+            
+            # Receive String
+            printstr = (yield)            
+            
+            # Line String
+            if online:
+                sys.stdout.write('\r' + ' '*clrlen)
+                sys.stdout.write('\r' + printstr)
+                clrlen = len(printstr)
+                sys.stdout.flush()
+    
+    # Closure
+    except GeneratorExit:
+        sys.stdout.write('\n')
+
+# Numeric Functions
+
+def get_trials(prob):
+	'''
+	Return the number of required trials
+	given a probability of success.
+	Internal use only.
+
+	:: prob
+	   type - float
+	   desc - probability of a rate event
+	'''
+	return 2 * int(ceil(
+		-10 / np.log(1. - prob)))
+
+def get_prob(success, trials):
+	'''
+	Return the probability of success given
+	the number of successes in some finite
+	trials. Internal use only.
+
+	:: success
+	   type - integer
+	   desc - total number of successes
+	:: trials
+	   type - integer
+	   desc - total number of attempts made
+	'''
+	return (success + 1.) / (trials + 2.)

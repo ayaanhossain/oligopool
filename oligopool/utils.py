05144190979cd35e131e7b14307bb515c5da51a1
ayaanhossain <ayaanhossain@noreply.github.com>
2020-11-23 23:10:21 -0500

    OligoPool Calc: Completed Draft of Primer Design Logic

--- oligopool/utils.py
+++ oligopool/utils.py
@@ -1,6 +1,10 @@
 import sys
-
+import os
+import shutil  as su
 import time    as tt
+
+import uuid    as uu
+import atexit  as ae
 
 import collections as cx
 
@@ -8,6 +12,27 @@ import numpy   as np
 import numba   as nb
 import primer3 as p3
 
+
+# Global Lookups
+complement_table = str.maketrans('ATGCUN', 'TACGAN')
+dna_alpha = set('ATGC')
+dna_space = {
+    'A': {'A'},
+    'C': {'C'},
+    'G': {'G'},
+    'T': {'T'},
+    'R': {'A', 'G'},
+    'Y': {'C', 'T'},
+    'S': {'G', 'C'},
+    'W': {'A', 'T'},
+    'K': {'G', 'T'},
+    'M': {'A', 'C'},
+    'B': {'C', 'G', 'T'},
+    'V': {'A', 'C', 'G'},
+    'D': {'A', 'G', 'T'},
+    'H': {'A', 'C', 'T'},
+    'N': {'A', 'T', 'G', 'C'}
+}
 
 # Decorators
 
@@ -263,6 +288,44 @@ def get_context_len(exmotifs):
 
     return len(exmotifs[-1]) - 1
 
+def get_context_type(context):
+    # All contexts unique
+    if isinstance(context, list):
+        return 1
+
+    # All contexts constant
+    elif isinstance(context, str):
+        return 2
+    
+    # No context
+    elif context == None:
+        return 3
+
+    # Unknown context
+    else:
+        return -1
+
+def get_context_num(context):
+
+    cntxnum = 0
+    
+    cntxtype = get_context_type(
+        context=context)
+
+    # All contexts unique
+    if cntxtype   == 1:
+        cntxnum = max(
+            cntxnum,
+            len(context))
+    
+    # All contexts constant
+    elif cntxtype == 2:
+        cntxnum = max(
+            cntxnum,
+            1)
+
+    return cntxnum
+
 def get_context_inference_fn(context):
     '''
     Return selector functions for building
@@ -275,17 +338,25 @@ def get_context_inference_fn(context):
               or None
     '''
 
+    # Get context type
+    cntxtype = get_context_type(context)
+
     # All contexts unique
-    if isinstance(context, list):
+    if cntxtype == 1:
         cifn = lambda x: context[x]
 
     # All contexts constant
-    elif isinstance(context, str):
+    elif cntxtype == 2:
         cifn = lambda x: context
     
     # No context
-    elif context == None:
+    elif cntxtype == 3:
         cifn = lambda x: ''
+
+    # Unknown packing
+    else:
+        raise ValueError(
+            'Context Packing Unknown')
 
     # Return Selector
     return cifn
@@ -293,7 +364,8 @@ def get_context_inference_fn(context):
 def get_motif_conflict(
     seq,
     seqlen,
-    exmotifs):
+    exmotifs,
+    partial):
     '''
     Determine if the barcode does not contain or is
     contained in one of the exluded motifs (motif
@@ -310,6 +382,12 @@ def get_motif_conflict(
        desc - list of motifs to exclude
               in designed barcodes,
               None otherwise
+    :: partial
+       type - boolean
+       desc - if True will not check for
+              conflicts for motifs longer
+              than seqlen, otherwise run
+              all checks
     '''
 
     # Do we have anything to exclude?
@@ -317,6 +395,11 @@ def get_motif_conflict(
 
         # Loop through all motifs
         for motif in exmotifs:
+
+            # Is sequence partial?
+            if partial:
+                if len(motif) > seqlen:
+                    break # No need to check further
 
             # Embedding Conflict
             if len(motif) <= seqlen and \
@@ -390,7 +473,8 @@ def get_assignment_index(
         mcond, motif = get_motif_conflict(
             seq=incntxseq,
             seqlen=len(incntxseq),
-            exmotifs=exmotifs)
+            exmotifs=exmotifs,
+            partial=False)
 
         # We have a winner folks!
         if mcond:
@@ -408,3 +492,366 @@ def get_assignment_index(
 
     # Failed Assignment
     return (False, None, cfails)
+
+def is_all_assignable(
+    seq,
+    exmotifs,
+    cifn,
+    cntxtype,
+    cntxnum,
+    cntxlen):
+    
+    aidx  = 0
+
+    while aidx < cntxnum:
+
+        if cntxtype == 0:
+            lcntx = cifn(aidx)[-cntxlen:]
+            incntxseq = lcntx + seq
+        else:
+            rcntx = cifn(aidx)[:+cntxlen]
+            incntxseq = seq + rcntx
+
+        # Determine context feasibility
+        mcond, motif = get_motif_conflict(
+            seq=incntxseq,
+            seqlen=len(incntxseq),
+            exmotifs=exmotifs,
+            partial=True)
+
+        if not mcond:
+            mstart = incntxseq.find(motif)
+
+            # Context on Left
+            if cntxtype == 0:
+                # print((lcntx, seq, 'left'))
+                tloc = len(motif) - len(lcntx) + mstart - 1
+            # Context on Right
+            else:
+                # print((seq, rcntx, 'right'))
+                tloc = mstart
+
+            return False, tloc
+
+        aidx += 1
+
+    return True, None
+
+# Sequence Analysis
+
+def is_DNA(seq):
+    '''
+    Determine if seq is a DNA string.
+    Internal use only.
+
+    :: seq
+       type - string
+       desc - a string in alphabet
+              {A, T, G, C}
+    '''
+
+    if isinstance(seq, str) and \
+       set(seq) <= dna_alpha:
+        return True
+    return False
+
+def get_comp(seq):
+    '''
+    Return the complement of seq.
+    Internal use only.
+
+    :: seq
+       type - string
+       desc - a string in alphabet
+              {A, T, G, C}
+    '''
+    
+    return seq.translate(complement_table)
+
+def get_revcomp(seq):
+    '''
+    Return the reverse complement of seq.
+    Internal use only.
+
+    :: seq
+       type - string
+       desc - a string in alphabet
+              {A, T, G, C}
+    '''
+    
+    return get_comp(seq)[::-1]
+
+def stream_spectrum(seq, k):
+    '''
+    Stream the k-mer spectrum of seq.
+    Internal use only.
+
+    :: seq
+       type - string
+       desc - a string in alphabet
+              {A, T, G, C}
+    :: k
+       type - integer
+       desc - k-mer length / k-value
+    '''
+    
+    return (seq[i:i+k] for i in range(len(seq)-k+1))
+
+def stream_canon_spectrum(seq, k):
+    '''
+    Stream the canonical k-mer spectrum
+    of seq. Internal use only.
+
+    :: seq
+       type - string
+       desc - a string in alphabet
+              {A, T, G, C}
+    :: k
+       type - integer
+       desc - k-mer length / k-value
+    '''
+    
+    return map(
+        lambda x: min(x, get_revcomp(x)),
+        stream_spectrum(seq=seq, k=k))
+
+# Workspace Functions
+def get_adjusted_path(path, suffix):
+    '''
+    Adjust outfile with suffix.
+    Internal use only.
+
+    :: path
+       type - string
+       desc - some path which must
+              have given suffix
+    :: suffix
+       type - string / None
+       desc - suffix to concatenate
+              with given path
+    '''
+
+    if path.endswith('/'):
+        path = path.rstrip('/')
+    if not suffix is None and \
+       not path.endswith(suffix):
+        path += str(suffix)
+    return path
+
+def get_path_status(
+    path,
+    suffix=None,
+    readable=True,
+    writable=False,
+    creatable=False):
+    '''
+    Arbitrary path evaluation.
+    Internal use only.
+
+    :: path
+       type - string
+       desc - path to store or output
+              information
+    :: suffix
+       type - string / None
+       desc - required path suffix
+              (default=None)
+    :: readable
+       type - boolean
+       desc - if True check is path is
+              readable  (file/dir   read)
+              (default=True)
+    :: writable
+       type - boolean
+       desc - if True check if path is
+              writable  (file/dir  write)
+              (default=False)
+    :: creatable
+       type - boolean
+       desc - if True check if path is
+              creatable (file/dir create)
+              (default=False)
+    '''
+
+    # Status:
+    # 0  = Path is invalid      input
+    # 1  = Path is unreadable   file
+    # 2  = Path is unwritable   file
+    # 3  = Path is empty        file
+    # 4  = Path is non-empty    file
+    # 5  = Path is unreadable   directory
+    # 6  = Path is unwritable   directory
+    # 7  = Path is empty        directory
+    # 8  = Path is non-empty    directory
+    # 9  = Path is non-existent
+    # 10 = Path is creatable
+    # 11 = Path is uncreatable
+    # X  = Path is existent     special file
+
+    # Is the path valid?
+    if not isinstance(path, str) or not path:
+        return 0 # only non-empty strings are paths
+
+    # Adjust Path for Checks
+    path = get_adjusted_path(
+        path=path,
+        suffix=suffix)
+    
+    # Is the path in existence?
+    if os.path.exists(path):
+        
+        # Path points to a file?
+        if os.path.isfile(path):
+
+            # Read accessible?
+            if  readable and not \
+                os.access(path, os.R_OK):
+                    return 1 # Unreadable file
+
+            # Is the file empty?
+            if not os.stat(path).st_size:
+
+                # Can we write to this file?
+                if  writable and not \
+                    os.access(path, os.W_OK):
+                        return 2 # Unwritable file
+                
+                return 3 # Empty file
+            
+            else:
+                return 4 # Non-empty file
+
+        # Path points to a directory?
+        elif os.path.isdir(path):
+
+            # What's the accessibility like?
+            if  readable and not \
+                os.access(path, os.R_OK):
+                    return 5 # Unreadable directory
+
+            # Directory empty?
+            if not any(os.scandir(path)):
+
+                # Can we write to this directory?
+                if  writable and not \
+                    os.access(path, os.W_OK):
+                        return 6 # Unwritable directory
+
+                return 7 # Empty Directory
+            
+            else:
+                return 8 # Non-empty Directory
+
+        # Path points to a special file such as
+        # socket, FIFO, device file etc.
+        else:
+            return 'X' # Special File Identifier
+
+    # Is the path creatable?
+    if creatable:
+
+        try:
+            # Build parent directory path
+            parentpath = os.path.dirname(path) or \
+                         os.getcwd()
+            # Try building parent path
+            os.makedirs(
+                name=parentpath,
+                exist_ok=True)
+        
+        except Exception as E:
+            return 11 # Cannot create path
+        
+        else:
+            return 10 # Can create path
+
+    # Path is non-existent
+    return 9
+
+def setup_directory(dirpath):
+    '''
+    Build dirpath. Internal use only.
+
+    :: dirpath
+       type - string
+       desc - a directory built to
+              store information
+    '''
+
+    dirpath = get_adjusted_path(
+        path=dirpath,
+        suffix=None)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+def setup_workspace(
+    outfile,
+    outfile_suffix):
+    '''
+    Adjust given output file and setup
+    output directory, and schedule its
+    removal at exit. Internal use only.
+
+    :: outfile
+       type - string
+       desc - output file
+    :: outfile_suffix
+       type - string
+       desc - output file suffix
+    '''
+
+    # Adjust outfile with suffix
+    outfile = get_adjusted_path(
+        path=outfile,
+        suffix=outfile_suffix)
+
+    # Define outdir
+    outdir = '.'.join([
+        outfile,
+        str(uu.uuid4()),
+        'dir'])
+
+    # Schedule outdir deletion
+    ae.register(
+        remove_directory,
+        outdir)
+
+    # Setup outdir
+    setup_directory(
+        dirpath=outdir)
+
+    # Return Workspace
+    return outfile, outdir
+
+def remove_file(filepath):
+    '''
+    Delete file stored at filepath.
+    Internal use only.
+
+    :: filepath
+       type - string
+       desc - a file to be removed
+    '''
+
+    filepath = get_adjusted_path(
+        path=filepath,
+        suffix=None)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+def remove_directory(dirpath):
+    '''
+    Delete dirpath and its contents.
+    Internal use only.
+
+    :: dirpath
+       type - string
+       desc - a directory (with content)
+              to be removed
+    '''
+    
+    dirpath = get_adjusted_path(
+        path=dirpath,
+        suffix=None)
+    if os.path.exists(dirpath):
+        su.rmtree(dirpath)

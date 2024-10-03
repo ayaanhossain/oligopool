d5af821741514e49647acdc5adf7dff9cbd4a99a
ayaanhossain <ayaanhossain@noreply.github.com>
2024-10-03 01:06:12 +0000

    updates

--- oligopool/__init__.py
+++ oligopool/__init__.py
@@ -39,7 +39,7 @@ interactively in a jupyter notebook, or be used to define
 scripts for design and analysis pipelines on the cloud.
 
 Oligopool Calculator offers two modes of operation
-    -   Design Mode for designing oligopool library
+    -   Design Mode for designing oligopool libraries
     - Analysis Mode for analyzing oligopool datasets
 
 Design Mode workflow
@@ -48,35 +48,14 @@ Design Mode workflow
         a. The DataFrame must contain a unique 'ID' column serving as primary key
         b. All other columns in the DataFrame must be DNA sequences or a - (dash)
     2. Next, define any optional background sequences via the background module
-    3. Add necessary oligopool elements with constraints via design modules
+    3. Add necessary oligopool elements with constraints via element modules
     4. Optionally, split long oligos and pad them via assembly modules
     5. Perform length checks as needed and finalize library via auxiliary modules
-
-    Example:
-    >>> import oligopool as op
-    >>>
-    >>> # Read initial library
-    >>> init_df = pd.read_csv('initial_library.csv')
-    >>>
-    >>> # Add oligo elements
-    >>> barcode_df, barcod_stats = op.barcode(input_data=init_df, ...)
-    >>> primer_df,  primer_stats = op.primer(input_data=barcode_df, ...)
-    >>>
-    >>> # Check length statistics
-    >>> op.lenstat(input_data=primer_df)
-    >>>
-    >>> # Split and pad longer oligos if needed
-    >>> split_df, split_stats = op.split(input_data=primer_df, ...)
-    >>> pad_df, pad_stats     = op.pad(input_data=split_df, ...)
-    >>>
-    >>> # Finalize and export library
-    >>> final_df, final_stats = op.final(input_data=split_df, ...)
-    >>> final_df.to_csv('final_library.csv')
 
     Background module available
         - background
 
-    Design modules available
+    Element modules available
         - primer
         - barcode
         - spacer
@@ -90,6 +69,30 @@ Design Mode workflow
         - lenstat
         - final
 
+    Worfklow example sketch
+
+    >>> import pandas as pd
+    >>> import oligopool as op
+    >>>
+    >>> # Read initial library
+    >>> df = pd.read_csv('initial_library.csv')
+    >>>
+    >>> # Add oligo elements
+    >>> barcode_df, stats = op.barcode(input_data=df, ...)
+    >>> primer_df,  stats = op.primer(input_data=barcode_df, ...)
+    ...
+    >>> # Check length statistics
+    >>> op.lenstat(input_data=primer_df)
+    >>>
+    >>> # Finalize the library
+    >>> final_df, stats = op.final(input_data=primer_df, ...)
+    >>>
+    >>> # Split and pad longer oligos if needed
+    >>> split_df, stats = op.split(input_data=final_df, ...)
+    >>> first_pad_df,  stats = op.pad(input_data=split_df, ...)
+    >>> second_pad_df, stats = op.pad(input_data=split_df, ...)
+    ...
+
 Analysis Mode workflow
 
     1. Index one or more CSVs containing the barcode and anchor information
@@ -97,27 +100,6 @@ Analysis Mode workflow
     3. If a barcode and its association with the core variant is to be counted use acount
     4. If multiple barcode combinations are to be counted use xcount
     5. Combine count matrices and perform stats and ML as necessary
-
-    Example:
-    >>> import oligopool as op
-    >>>
-    >>> # Read marked up library
-    >>> final_df = pd.read_csv('final_library.csv')
-    >>>
-    >>> # Index the barcodes and save the indexes
-    >>> bc1_index_stats = op.index(input_data=final_df, barcode_column='BC1', ...)
-    >>> bc2_index_stats = op.index(input_data=final_df, barcode_column='BC2', ...)
-    >>>
-    >>> # Pack the FastQ files
-    >>> sam1_pack_stats = op.pack(r1_file='sample_1_R1.fq.gz', ...)
-    >>> sam2_pack_stats = op.pack(r1_file='sample_2_R1.fq.gz', ...)
-    >>>
-    >>> # Compute and write the barcode combination count matrix
-    >>> xount_stats = op.xcount(index_files=['bc1_index', 'bc2_index'],
-    ...                         pack_file='sample_1_pack', ...)
-    >>>
-    >>> # Read the count matrix and continue analysis
-    >>> count_df = pd.read_csv('library_count_matrix')
 
     Indexing module available
         - index
@@ -128,6 +110,33 @@ Analysis Mode workflow
     Counting modules available
         - acount
         - xcount
+
+    Anamysis Mode worfklow example sketch
+
+    >>> import pandas as pd
+    >>> import oligopool as op
+    >>>
+    >>> # Read marked up library
+    >>> df = pd.read_csv('oligo_library.csv')
+    >>>
+    >>> # Index the barcodes and save the indexes
+    >>> bc1_index_stats = op.index(input_data=df, barcode_column='BC1', ...)
+    >>> bc2_index_stats = op.index(input_data=df, barcode_column='BC2', ...)
+    ...
+    >>>
+    >>> # Pack the FastQ files
+    >>> sam1_pack_stats = op.pack(r1_file='sample_1_R1.fq.gz', ...)
+    >>> sam2_pack_stats = op.pack(r1_file='sample_2_R1.fq.gz', ...)
+    ...
+    >>>
+    >>> # Compute and write the barcode combination count matrix
+    >>> xcount_stats = op.xcount(index_files=['bc1_index', 'bc2_index'],
+    ...                          pack_file='sample_1_pack', ...)
+    ...
+    >>>
+    >>> # Read the count matrix and continue analysis
+    >>> count_df = pd.read_csv('sample_1_counts.csv')
+    ...
 
 You can learn more about each module using help.
 >>> import oligopool as op
